7a18509b39b11123619b2b2e9cdc153c000ced74
Chris DeForest <ced6563@rit.edu>
2025-09-10 16:25:12 -0400

    added token constants

--- src/com/project/JottTokenizer.java
+++ src/com/project/JottTokenizer.java
@@ -9,6 +9,10 @@ package project;
 import java.util.ArrayList;
 
 public class JottTokenizer {
+  // constants do not include numbers or letters, that would just be dumb
+  private static final String WHITESPACE = " ", NEWLINE = "\n", COMMENT = "#", COMMA = ",", RBRACKET = "]", LBRACKET = "[", RBRACE = "}", LBRACE = "{";
+  private static final String EQUALS = "=", LRARROW = "<>", MULTIPLY = "*", DIVIDE = "/", ADD = "+", SUBTRACT = "-", SEMICOLON = ";", DECIMAL = ".";
+  private static final String COLON = ":", EXCLAMATION = "!", QUOTE = "\"";
 
 	/**
      * Takes in a filename and tokenizes that file into Tokens
@@ -16,7 +20,11 @@ public class JottTokenizer {
      * @param filename the name of the file to tokenize; can be relative or absolute path
      * @return an ArrayList of Jott Tokens
      */
-    public static ArrayList<Token> tokenize(String filename){
+  public static ArrayList<Token> tokenize(String filename){
 		return null;
 	}
 }
+
+// any error, report to System.err, return NULL
+// make repetitive code into helper functions when possible
+
