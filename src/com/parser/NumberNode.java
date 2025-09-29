939bd5c0edf46d50d2f977840d69ad8b47e4adda
Chris DeForest <ced6563@rit.edu>
2025-09-29 15:44:21 -0400

    started adding parser classes

---
+++ src/com/parser/NumberNode.java
@@ -0,0 +1,22 @@
+package parser;
+
+import provided.JottTree;
+
+
+public class NumberNode implements JottTree {
+    public String convertToJott() {
+        return null;
+    }
+    public String convertToJava(String classname) {
+        return null;
+    }
+    public String convertToC() {
+        return null;
+    }
+    public String convertToPython() {
+        return null;
+    }
+    public boolean validateTree() {
+        return false;
+    }
+}
---
+++ src/com/parser/OperandNode.java
@@ -0,0 +1,21 @@
+package parser;
+
+import provided.JottTree;
+
+public class OperandNode implements JottTree {
+    public String convertToJott() {
+        return null;
+    }
+    public String convertToJava(String classname) {
+        return null;
+    }
+    public String convertToC() {
+        return null;
+    }
+    public String convertToPython() {
+        return null;
+    }
+    public boolean validateTree() {
+        return false;
+    }
+}
