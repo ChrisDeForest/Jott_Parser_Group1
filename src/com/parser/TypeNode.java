dcfc04320198107d5ac6980791a00ec90feda3d4
ak6169 <ak6169@rit.edu>
2025-10-15 16:26:22 -0400

    implemented TypeNode.java

--- src/com/parser/TypeNode.java
+++ src/com/parser/TypeNode.java
@@ -1,19 +1,45 @@
 package parser;
+
+import provided.Token;
+import provided.TokenType;
 import provided.JottTree;
+import java.util.ArrayList;
 
+public class TypeNode implements JottTree {
+    private final Token typeToken;
 
-public class TypeNode implements JottTree{
-    
-    public TypeNode() {
+    public TypeNode(Token typeToken) {
+        this.typeToken = typeToken;
     }
 
-    public static TypeNode parseTypeNode() {
-        return new TypeNode();
+    public static TypeNode parseTypeNode(ArrayList<Token> tokens) {
+        if (tokens == null || tokens.size() == 0) {
+            System.err.println("parseTypeNode: expected type but no tokens available");
+            return null;
+        }
+        Token t = tokens.get(0);
+        if (t.getTokenType() != TokenType.ID_KEYWORD) {
+            System.err.println("parseTypeNode: expected type keyword, got '" + t.getToken() + "' at " + t.getFilename() + ":" + t.getLineNum());
+            return null;
+        }
+
+        // Check if the token is a valid type: Double, Integer, String, or Boolean
+        String tokenValue = t.getToken();
+        if (!tokenValue.equals("Double") && !tokenValue.equals("Integer") && 
+            !tokenValue.equals("String") && !tokenValue.equals("Boolean")) {
+            System.err.println("parseTypeNode: expected type (Double, Integer, String, or Boolean), got '" + tokenValue + "' at " + t.getFilename() + ":" + t.getLineNum());
+            return null;
+        }
+
+        // consume the token and return a new TypeNode
+        tokens.remove(0);
+        return new TypeNode(t);
     }
 
     @Override
     public String convertToJott() {
-        return null;
+        // returns a string representation of the type token
+        return typeToken.getToken();
     }
 
     @Override
@@ -35,5 +61,4 @@ public class TypeNode implements JottTree{
     public boolean validateTree() {
         return false;
     }
-    
 }
