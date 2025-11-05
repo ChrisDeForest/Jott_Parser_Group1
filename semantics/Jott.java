0c050ebab80c96623ffcf1e50f33d0d0fd6f75f3
Chris DeForest <ced6563@rit.edu>
2025-11-05 16:19:43 -0500

    jott.java implemented

--- semantics/Jott.java
+++ semantics/Jott.java
@@ -1,5 +1,29 @@
 package semantics;
 
+import provided.JottTokenizer;
+import provided.JottTree;
+import provided.JottParser;
+import provided.Token;
+import parser.ParseException;
+
+import java.util.ArrayList;
+
 public class Jott {
-    
+    public static void main(String[] args){
+        try {
+            ArrayList<Token> tokens = JottTokenizer.tokenize(args[0]);
+            JottTree root = JottParser.parse(tokens);
+            root.validateTree();
+            System.exit(0);
+        } catch (SemanticException se) {
+            System.err.println(se.getMessage());
+            System.exit(1);
+        } catch (ParseException pe) {
+            System.err.println(pe.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
